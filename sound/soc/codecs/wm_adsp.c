e4df7897f73609842c647ee1b7bb8c9e19247464
Patrick Gries <patgries@motorola.com>
2019-06-20 23:49:05 -0500

    Revert "(CR): ASoC: wm_adsp: Change kcontrol for AoU and AoU WoA"
    WoA is also known as Wake on Approach (Cirrus definition)
    
    This reverts commit 452f77cace4b945fe82c315b2b39f7014af113c6.
    CRs-fixed: (CR): Olson: revert AoU Concurrent feature
    
    Change-Id: I1f0eb8787f24633b1d085bfd40d57a4b2d2c8a51
    Reviewed-on: https://gerrit.mot.com/1374421
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Yuesheng Huang <huangys@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -511,10 +511,8 @@ static void wm_adsp_buf_flash(struct list_head *list, void *dest)
 #define WM_ADSP_FW_TRACE    8
 #define WM_ADSP_FW_SPK_PROT 9
 #define WM_ADSP_FW_MISC     10
-#define WM_ADSP_FW_AOU      11
-#define WM_ADSP_FW_AOU_CONCURR     12
 
-#define WM_ADSP_NUM_FW      13
+#define WM_ADSP_NUM_FW      11
 
 static const char *wm_adsp_fw_text[WM_ADSP_NUM_FW] = {
 	[WM_ADSP_FW_MBC_VSS] =  "MBC/VSS",
@@ -528,8 +526,6 @@ static const char *wm_adsp_fw_text[WM_ADSP_NUM_FW] = {
 	[WM_ADSP_FW_TRACE] =    "Dbg Trace",
 	[WM_ADSP_FW_SPK_PROT] = "Protection",
 	[WM_ADSP_FW_MISC] =     "Misc",
-	[WM_ADSP_FW_AOU] =     "AoU",
-	[WM_ADSP_FW_AOU_CONCURR] = "AoU Audio",
 };
 
 struct wm_adsp_system_config_xm_hdr {
@@ -697,9 +693,7 @@ static struct wm_adsp_fw_defs wm_adsp_fw[WM_ADSP_NUM_FW] = {
 		.caps = trace_caps,
 	},
 	[WM_ADSP_FW_SPK_PROT] = { .file = "spk-prot" },
-	[WM_ADSP_FW_MISC] = { .file = "misc" },
-	[WM_ADSP_FW_AOU] = { .file = "aou-ultrasound" },
-	[WM_ADSP_FW_AOU_CONCURR] = { .file = "aou-concurrent" },
+	[WM_ADSP_FW_MISC] =     { .file = "misc" },
 };
 
 struct wm_coeff_ctl_ops {
@@ -939,10 +933,8 @@ static int wm_adsp_fw_put(struct snd_kcontrol *kcontrol,
 	if (ucontrol->value.enumerated.item[0] == dsp[e->shift_l].fw)
 		return 0;
 
-	if (ucontrol->value.enumerated.item[0] >= WM_ADSP_NUM_FW) {
-		adsp_dbg(dsp, "Firmware error 1\n");
+	if (ucontrol->value.enumerated.item[0] >= WM_ADSP_NUM_FW)
 		return -EINVAL;
-	}
 
 	mutex_lock(&dsp[e->shift_l].pwr_lock);
 
