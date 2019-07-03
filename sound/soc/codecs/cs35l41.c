b4e2408b8a684474fe0426f3c9aa83cee2ee6c90
huangys <huangys@motorola.com>
2019-07-03 20:26:46 +0800

    ASoC: cs35l41: firmware preload on boot up
    set preloaded to true on bootup for loading firmware as early as
    possible. As the default value is true, audio route think this value
    is not changed, so It's not necessary to set.
    
    Change-Id: I43d6f51579572f391d455fe3adf344fb25a6688d
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1381036
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- sound/soc/codecs/cs35l41.c
+++ sound/soc/codecs/cs35l41.c
@@ -2169,6 +2169,7 @@ static int cs35l41_dsp_init(struct cs35l41_private *cs35l41)
 	dsp->mem = cs35l41_dsp1_regions;
 	dsp->num_mems = ARRAY_SIZE(cs35l41_dsp1_regions);
 	dsp->unlock_all = true;
+	dsp->preloaded = true;
 
 	dsp->n_rx_channels = CS35L41_DSP_N_RX_RATES;
 	dsp->n_tx_channels = CS35L41_DSP_N_TX_RATES;
