bb184e8a3f1408a1fd0343ac1f78c6b5c0a7dada
James Schulman <james.schulman@cirrus.com>
2019-07-15 16:19:29 -0500

    ASoC: cs35l45: Support for CS35L45 amplifier
    Add support for Cirrus Logic CS35L45 boosted smart amplifier.
    
    Change-Id: Ib348089cf6b3ffca130832e86abffb8ec9963cb8
    Signed-off-by: James Schulman <james.schulman@cirrus.com>

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -7,13 +7,12 @@ LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(TOP)/vendor/qcom/opensource/core-utils/dlkm/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
-ifeq ($(CIRRUS_AMP_CODEC),cs35l41_spi)
-KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L41_SPI=y
+ifeq ($(CIRRUS_AMP_CODEC),cs35l45_spi)
+KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L45_SPI=y
 else
-KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L41_I2C=y
+KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L45_I2C=y
 endif
-LOCAL_MODULE := cirrus_cs35l41.ko
+LOCAL_MODULE := cirrus_cs35l45.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(TOP)/vendor/qcom/opensource/core-utils/dlkm/AndroidKernelModule.mk
-
