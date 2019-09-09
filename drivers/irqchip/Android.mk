b896d04679a4a6705bcbfe3fd625722da912d538
huangys <huangys@motorola.com>
2019-09-09 13:01:37 +0800

    mfd: madera: use CS47L35 macro to seperate
    Racer lite audio birngup, code change for sharing between cs47l35
    and cs47l90
    
    Change-Id: Ic00ac5feb8b9df2f94bb61333d672aa103574d63
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1415001
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- drivers/irqchip/Android.mk
+++ drivers/irqchip/Android.mk
@@ -2,6 +2,11 @@ DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+ifeq ($(CIRRUS_CODEC),cs47l35)
+KERNEL_CFLAGS += CONFIG_SND_SOC_CS47L35=y
+else
+KERNEL_CFLAGS += CONFIG_SND_SOC_CS47L90=y
+endif
 LOCAL_MODULE := cirrus_irq-madera.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
