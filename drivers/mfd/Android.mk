bf0b972e62d5b48e9e02af6a9ddaf97fec6588d8
huangys <huangys@motorola.com>
2019-09-19 18:40:01 +0800

    mfd/irqchip build script
    It's not a goode idea for seperation using product name or build
    option. we can define CS47L35 and CS47L90 together, build both of
    them to kernel module. distingusish them by madera type which is
    defined in devcie tree at runtime.
    
    Change-Id: I7044d7101067e120edb25e6e5ccccd8c4ec0863c
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1424615
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Submit-Approved: Jira Key

--- drivers/mfd/Android.mk
+++ drivers/mfd/Android.mk
@@ -2,11 +2,6 @@ DLKM_DIR := motorola/kernel/modules
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-ifeq ($(CIRRUS_CODEC),cs47l35)
-KERNEL_CFLAGS += CONFIG_SND_SOC_CS47L35=y
-else
-KERNEL_CFLAGS += CONFIG_SND_SOC_CS47L90=y
-endif
 
 LOCAL_MODULE := cirrus_madera-core.ko
 LOCAL_MODULE_TAGS := optional
