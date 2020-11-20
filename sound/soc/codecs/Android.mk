48255e2dac23be29de9c02c9ae629f56b657a53c
Vlad Karpovich <Vlad.Karpovich@cirrus.com>
2020-11-20 14:14:14 -0600

    ASoC: cs42l42: Add Android build files
    The driver will be built as a kernel module outside
    of kernel tree as part of Android build.
    
    Mot-CRs-Fixed: (CR)
    
    Signed-off-by: Vlad Karpovich <Vlad.Karpovich@cirrus.com>
    Change-Id: I610677d10b052fa79891cc00d636513aa9aedfdb

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -4,14 +4,22 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 ifneq (,$(filter ironmn%, $(TARGET_PRODUCT)))
-#include $(CLEAR_VARS)
 ifeq ($(CIRRUS_CS35L45_CODEC),i2c)
 KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L45_I2C=y
 else
 KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L45_SPI=y
 endif
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := cirrus_cs35l45.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cirrus_cs42l42.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
 endif
