f58300cf455f03c3d39089fe2de0d53d9027950a
Nelson Guedes Paulo Junior <npaulo@motorola.com>
2021-04-21 12:16:44 +0800

    cs35l45: only compile cs35l45 for specific product
    Only allow compilation of cs35l45 drivers for specific products.
    In this case we are adding only ironman since other products on
    same repo doesn't use Cirrus drivers.
    
    Change-Id: I6a4de5a209b4d3f0fc5b30a7fab6801d9f1b8317
    Reviewed-on: https://gerrit.mot.com/2078985
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key
    Tested-by: Jira Key
    Reviewed-by: Yinjun Chen <a7301c@motorola.com>

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -3,18 +3,15 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := cirrus_wm_adsp.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
-ifeq ($(CIRRUS_AMP_CODEC),cs35l45_spi)
-KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L45_SPI=y
-else
+ifneq (,$(filter ironmn%, $(TARGET_PRODUCT)))
+#include $(CLEAR_VARS)
+ifeq ($(CIRRUS_CS35L45_CODEC),i2c)
 KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L45_I2C=y
+else
+KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L45_SPI=y
 endif
 LOCAL_MODULE := cirrus_cs35l45.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
+endif
