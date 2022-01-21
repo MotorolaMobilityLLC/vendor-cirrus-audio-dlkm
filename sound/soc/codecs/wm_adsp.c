9323107083a033bcf6a334c87400d7c1a3ee2c6e
Vlad Karpovich <Vlad.Karpovich@cirrus.com>
2022-01-20 18:28:43 -0600

    ASoC: wm_adsp: Add trace caps to speaker protection FW
    Enable access to the speaker protection firmware debug
    stream using compress stream API.
    Lower minimum fragment size to 16 words.
    
    Change-Id: Ifd08fdf9f2a244b70fea9cc66de1b25148381fce
    Signed-off-by: Vlad Karpovich <Vlad.Karpovich@cirrus.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -484,7 +484,7 @@ struct wm_adsp_compr {
 
 #define WM_ADSP_MIN_FRAGMENTS		1
 #define WM_ADSP_MAX_FRAGMENTS		256
-#define WM_ADSP_MIN_FRAGMENT_SIZE_WORDS	64
+#define WM_ADSP_MIN_FRAGMENT_SIZE_WORDS	16
 #define WM_ADSP_MAX_FRAGMENT_SIZE_WORDS	4096
 
 #define WM_ADSP_ALG_XM_STRUCT_MAGIC    0x49aec7
@@ -594,7 +594,12 @@ static const struct {
 		.num_caps = ARRAY_SIZE(trace_caps),
 		.caps = trace_caps,
 	},
-	[WM_ADSP_FW_SPK_PROT] = { .file = "spk-prot" },
+	[WM_ADSP_FW_SPK_PROT] = {
+		.file = "spk-prot",
+		.compr_direction = SND_COMPRESS_CAPTURE,
+		.num_caps = ARRAY_SIZE(trace_caps),
+		.caps = trace_caps,
+	},
 	[WM_ADSP_FW_SPK_CALI] = { .file = "spk-cali" },
 	[WM_ADSP_FW_SPK_DIAG] = { .file = "spk-diag" },
 	[WM_ADSP_FW_MISC] =     { .file = "misc" },
